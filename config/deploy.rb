fd1efb5fb2e7537c51113c0ed73f39a1f6f298f8
Artem <q3pp@yandex.ru>
2016-01-14 23:08:55 +0300

    fix deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,7 @@ set :unicorn_pid,    "/var/run/unicorn/#{fetch(:user)}/" \
 set :bundle_without, [:development, :test]
 set :use_sudo,       false
 
-set :repo_url,       "https://github.com/q3pp/ToDo"
+set :repo_url,       "git@github.com:q3pp/ToDo.git"
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
