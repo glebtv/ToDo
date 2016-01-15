1a3493583b98fd5b68a3ac9201a1f8ba0cddc694
Artem <q3pp@yandex.ru>
2016-01-15 09:11:15 +0300

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     if @user == current_user
-      @visible_tasks = current_user.created_tasks
-      @assigned_tasks = current_user.tasks
+      @authored_task = current_user.created_tasks
+      @visible_tasks = current_user.tasks
     else
       @authored_task = @user.tasks.authored(current_user)
       @visible_tasks = @user.tasks.visible_tasks(current_user)
